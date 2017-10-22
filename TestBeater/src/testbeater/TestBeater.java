3b270c104315d6fee4cde6add93f84d5802c0702
Jhonaroas <jhonaroas@gmail.com>
2017-10-22 16:04:54 -0500

    Holi

--- TestBeater/src/testbeater/TestBeater.java
+++ TestBeater/src/testbeater/TestBeater.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package testbeater;
 
 /**
@@ -17,7 +13,7 @@ public class TestBeater {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Hola mundo que hace");
+        System.out.println("Holi moly");
     }
     
 }
